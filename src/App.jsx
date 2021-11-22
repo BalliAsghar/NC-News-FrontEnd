31c3b9c3a9750e6c2655b7f57a66b0a451a6a6a1
BalliAsghar <ballihacker@outlook.com>
2021-11-22 14:58:36 +0000

    basic navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,10 @@
+import Navbar from "./components/Narbar";
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div>
+      <Navbar />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/Narbar.jsx
@@ -0,0 +1,11 @@
+const Narbar = () => {
+  return (
+    <div className="w-screen flex flex-row items-center p-1 justify-between bg-white shadow-xs">
+      <div className="flex flex-row items-center">
+        <h1 className="text-3xl font-bold text-gray-800">Nc-News</h1>
+      </div>
+    </div>
+  );
+};
+
+export default Narbar;
