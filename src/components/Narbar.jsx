99424c2b394483b87dbd8324da25369e5ec54cc2
BalliAsghar <ballihacker@outlook.com>
2021-11-22 15:15:21 +0000

    Navbar

--- src/components/Narbar.jsx
+++ src/components/Narbar.jsx
@@ -1,10 +1,15 @@
 const Narbar = () => {
   return (
-    <div className="w-screen flex flex-row items-center p-1 justify-between bg-white shadow-xs">
-      <div className="flex flex-row items-center">
-        <h1 className="text-3xl font-bold text-gray-800">Nc-News</h1>
+    <header className="text-gray-400 bg-white body-font">
+      <div className="container mx-auto flex flex-wrap p-2  flex-col md:flex-row items-center">
+        <a className="flex order-first lg:order-none lg:w-1/5 title-font font-medium items-center text-white lg:items-center lg:justify-center mb-4 md:mb-0"></a>
+        <div className="lg:w-2/5 inline-flex lg:justify-end ml-5 lg:ml-0">
+          <h1 className="text-7xl text-center text-gray-700 raleway">
+            Nc-News
+          </h1>
+        </div>
       </div>
-    </div>
+    </header>
   );
 };
 
