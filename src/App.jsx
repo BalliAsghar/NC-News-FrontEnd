df0d3b543861f79a1f1001f926bc900b51ec3355
BalliAsghar <ballihacker@outlook.com>
2021-11-22 15:55:11 +0000

    Home Page Setup

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,12 @@ function App() {
   return (
     <div>
       <Navbar />
+      <div className="ml-5 p-4">
+        <button className=" bg-transparent raleway font-semibold text-5xl  py-5 px-10 box-border cursor-default border border-gray-600 rounded hover:translate-x-1 hover:bg-gray-900 hover:text-white">
+          HEADLINES
+        </button>
+        {/* Headlines Here */}
+      </div>
     </div>
   );
 }
