2a11a0738f6f3e203749c40198112ebe1050e35e
BalliAsghar <ballihacker@outlook.com>
2021-11-26 20:25:52 +0000

    grid now

--- src/components/Headlines.jsx
+++ src/components/Headlines.jsx
@@ -23,63 +23,63 @@ const Headlines = () => {
     <div>
       {<Topics setTopic={changeTopic} />}
       <div className="container mx-auto my-auto">
-        <div className="grid grid-cols-3 gap-0 xs:grid-cols-1 md:grid-cols-2 xl:grid-cols-3  ">
+        <div className="grid grid-cols-1 gap-2 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-3">
           {articles.map((article) => {
             return (
               <div
-                class="mx-auto px-4 py-8 max-w-xl my-5"
+                className="mx-auto px-4 py-8 max-w-xl my-3"
                 key={article.article_id}
               >
-                <div class="bg-white shadow-2xl rounded-lg mb-6 tracking-wide">
-                  <div class="md:flex-shrink-0">
+                <div className="bg-white shadow-2xl rounded-lg mb-6 tracking-wide">
+                  <div className="md:flex-shrink-0">
                     <img
                       src={article.image_url}
                       alt="mountains"
-                      class="w-full h-64 rounded-lg rounded-b-none"
+                      className="w-full h-64 rounded-lg rounded-b-none"
                     />
                   </div>
-                  <div class="px-4 py-2 mt-2">
-                    <h2 class="font-bold text-2xl text-gray-800 tracking-normal">
+                  <div className="px-4 py-2 mt-2">
+                    <h2 className="font-bold text-2xl text-gray-800 tracking-normal">
                       {article.title}
                     </h2>
-                    <p class="text-sm text-gray-700 px-2 mr-1">
+                    <p className="text-sm text-gray-700 px-2 mr-1">
                       Lorem ipsum dolor sit amet consectetur adipisicing elit.
                       Tempora reiciendis ad architecto at aut placeat quia,
                       minus dolor praesentium officia maxime deserunt porro amet
                       ab debitis deleniti modi soluta similique...
                     </p>
-                    <div class="flex items-center justify-between mt-2 mx-6">
-                      <a href="#" class="text-blue-500 text-xs -ml-3 ">
+                    <div className="flex items-center justify-between mt-2 mx-6">
+                      <a href="#" className="text-blue-500 text-xs -ml-3 ">
                         Show More
                       </a>
-                      <a href="#" class="flex text-gray-700">
+                      <a href="#" className="flex text-gray-700">
                         <svg
                           fill="none"
                           viewBox="0 0 24 24"
-                          class="w-6 h-6 text-blue-500"
+                          className="w-6 h-6 text-blue-500"
                           stroke="currentColor"
                         >
                           <path
-                            stroke-linecap="round"
-                            stroke-linejoin="round"
-                            stroke-width="2"
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            strokeWidth="2"
                             d="M8 10h.01M12 10h.01M16 10h.01M9 16H5a2 2 0 01-2-2V6a2 2 0 012-2h14a2 2 0 012 2v8a2 2 0 01-2 2h-5l-5 5v-5z"
                           />
                         </svg>
                         {article.comment_count}
                       </a>
                     </div>
-                    <div class="author flex items-center -ml-3 my-3">
-                      <div class="user-logo">
+                    <div className="author flex items-center -ml-3 my-3">
+                      <div className="user-logo">
                         <img
-                          class="w-12 h-12 object-cover rounded-full mx-4  shadow"
+                          className="w-12 h-12 object-cover rounded-full mx-4  shadow"
                           src="https://source.unsplash.com/random"
                           alt="avatar"
                         />
                       </div>
-                      <h2 class="text-sm tracking-tighter text-gray-900">
+                      <h2 className="text-sm tracking-tighter text-gray-900">
                         <a href="#">By {article.author}</a>{" "}
-                        <span class="text-gray-600">21 SEP 2015.</span>
+                        <span className="text-gray-600">21 SEP 2015.</span>
                       </h2>
                     </div>
                   </div>
