c32beba7397408ea22257176d2d386625c77a0ef
BalliAsghar <ballihacker@outlook.com>
2021-11-28 18:49:57 +0000

    comment section

--- src/components/AddComments.jsx
+++ src/components/AddComments.jsx
@@ -1,4 +1,4 @@
-import { useState, useContext } from "react";
+import { useState, useContext, useEffect } from "react";
 import NewsServiceApi from "../services/api";
 import UserContext from "../context/User.Context";
 const AddComments = ({ articleId }) => {
@@ -7,9 +7,10 @@ const AddComments = ({ articleId }) => {
 
   const { user } = useContext(UserContext);
 
+  useEffect(() => {}, [setComment]);
+
   const handleSubmit = async (e) => {
     e.preventDefault();
-    console.log("hello");
     try {
       await NewsServiceApi.postCommentOnArticle(
         articleId,
@@ -23,60 +24,52 @@ const AddComments = ({ articleId }) => {
     }
   };
 
+  if (!user) return null;
+
   return (
-    <div className="flex mx-auto items-center justify-center shadow-lg mb-4 py-2 max-w-lg">
-      <form
-        className="w-full max-w-xl bg-white px-4 pt-2"
-        onSubmit={handleSubmit}
-      >
-        <div className="flex flex-wrap -mx-3 mb-6" />
-        <h2 className="px-4 pt-3 pb-2 text-gray-800 text-lg">
-          Add a new comment
-        </h2>
-        <div className="w-full md:w-full px-3 mb-2 mt-2 ">
-          <textarea
-            className="bg-gray-100 rounded border border-gray-400 leading-normal resize-none w-full h-20 py-2 px-3 font-medium placeholder-gray-700 focus:outline-none focus:bg-white"
-            name="body"
-            placeholder="Type Your Comment"
-            required
-            value={comment}
-            onChange={(e) => setComment(e.target.value)}
-          ></textarea>
-        </div>
-        <div className="w-full flex items-start md:w-full px-3">
-          {message && (
-            <div className="flex items-start w-1/2 text-green-700 font-semibold px-2 mr-auto">
-              {message}
+    <div className="flex justify-center items-center">
+      <div className="w-1/2 bg-white p-2 pt-4 rounded shadow-lg rounded-lg">
+        <form onSubmit={handleSubmit}>
+          <div className="flex ml-3">
+            <div className="mr-3">
+              <img
+                src={user.user.avatar_url}
+                width="50px"
+                height="50px"
+                alt=""
+                className="rounded-full"
+              />
             </div>
-          )}
-          {!user && (
-            <div className="flex items-start w-1/2 text-gray-700 px-2 mr-auto">
-              <svg
-                fill="none"
-                className="w-5 h-5 text-gray-600 mr-1"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
-                />
-              </svg>
-              <p className="text-xs md:text-sm pt-px">You must be logged In</p>
+            <div>
+              <h1 className="font-semibold">{user.user.username}</h1>
+              <p className="text-xs text-gray-500">{user.user.name}</p>
             </div>
-          )}
-          <div className="-mr-1">
-            <input
-              type="submit"
-              className="bg-white text-gray-700 font-medium py-1 px-4 border border-gray-400 rounded-lg tracking-wide mr-1 hover:bg-gray-100"
-              value="Post Comment"
-              disabled={user ? false : true}
-            />
           </div>
-        </div>
-      </form>
+
+          <div className="mt-3 p-3 w-full">
+            <textarea
+              rows="3"
+              className="border p-2 rounded w-full"
+              placeholder="Write something..."
+              value={comment}
+              onChange={(e) => setComment(e.target.value)}
+            ></textarea>
+          </div>
+
+          <div className="flex justify-between mx-3 gap-2">
+            <div>
+              <button
+                type="submit"
+                disabled={!comment}
+                className="px-4 py-1 bg-gray-800 text-white rounded font-light hover:bg-gray-700"
+              >
+                Submit
+              </button>
+            </div>
+            {message && <div className="text-xl text-green-900">{message}</div>}
+          </div>
+        </form>
+      </div>
     </div>
   );
 };
--- src/components/Article.jsx
+++ src/components/Article.jsx
@@ -14,6 +14,7 @@ const Article = () => {
   if (!loading) {
     return (
       <div className="m-2 p-2 mt-14">
+        <img src={article.image_url} alt="Image" />
         <h1 className="font-mono text-3xl font-extrabold text-blue-600">
           {article.title}
         </h1>
